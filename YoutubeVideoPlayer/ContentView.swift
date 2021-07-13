bdd441b41f19096843ee23f5d6fff838aba6be79
Raphael Cerqueira <raphaelfc@gmail.com>
2021-07-13 17:00:28 -0300

    Initial Commit

---
+++ YoutubeVideoPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YoutubeVideoPlayer
+//
+//  Created by Raphael Cerqueira on 13/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
